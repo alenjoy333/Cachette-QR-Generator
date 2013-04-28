da02ecb90f46b583b97b05fd65383195c155f39f
Alen Joy <alenjoy333@gmail.com>
2013-04-28 18:43:22 +0530

    Final Commit

---
+++ Cachette_QR_Code_Generator/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Windows.Media.Imaging;
+using com.google.zxing;
+using com.google.zxing.qrcode;
+
+namespace Cachette_QR_Code_Generator
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void qrCodeGenerator(object sender, RoutedEventArgs e)
+        {
+            if (txtQRCode.Text != "")
+            {
+                lblError.Text = "";
+                int qrWidth = 400;
+                int qrHeight = 400;
+                string qrCodeInput = txtQRCode.Text;
+                QRCodeWriter generator = new QRCodeWriter();
+                var qrMatrix = generator.encode(qrCodeInput, BarcodeFormat.QR_CODE, qrWidth, qrHeight);
+                WriteableBitmap qrCodeImage = new System.Windows.Media.Imaging.WriteableBitmap(qrWidth, qrHeight);
+
+                for (int y = 0; y < qrHeight; y++)
+                {
+                    for (int x = 0; x < qrWidth; x++)
+                    {
+                        int grayValue = qrMatrix.Array[y][x] & 0xff;
+                        if (grayValue == 0)
+                        {
+                            qrCodeImage.SetPixel(x, y, Color.FromArgb(255, 0, 0, 0));
+                        }
+                        else
+                        {
+                            qrCodeImage.SetPixel(x, y, Color.FromArgb(255, 255, 255, 255));
+                        }
+                    }
+                }
+                imgQRCode.Source = qrCodeImage;
+            }
+            else
+            {
+                imgQRCode.Source = null;
+                lblError.Text = "You Have To Enter A String To Generate QR Image";
+               
+
+            }
+        }
+
+    }
+}
---
+++ Cachette_QR_Code_Generator/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cachette_QR_Code_Generator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cachette_QR_Code_Generator")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2f3667ac-4585-4a9f-b6cf-adeda4ce02b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
